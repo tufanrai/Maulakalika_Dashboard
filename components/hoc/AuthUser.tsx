8cc151928f027f03bcbd393e324027fcc37c8108
TufanRai <thulungtufan16@gmail.com>
2025-11-23 16:32:25 +0545

    update: Displayed the data fetched from the database

---
+++ components/hoc/AuthUser.tsx
@@ -0,0 +1,40 @@
+"use client";
+import React from "react";
+import { jwtDecode } from "jwt-decode";
+import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
+import toast from "react-hot-toast";
+
+function WithAuthorization<T>(
+  Component: React.ComponentType<T>,
+  roles: string[]
+) {
+  return function WithAuthentication(props: any) {
+    // login
+    const router = useRouter();
+    React.useEffect(() => {
+      const token = Cookies.get("accessToken")!;
+      const decoded: { exp: number; role: string } = jwtDecode(token);
+
+      if (decoded.exp <= Math.floor(Date.now() / 1000)) {
+        toast.error("session expired: please login");
+        Cookies.remove("accessToken");
+        setTimeout(() => {
+          router.replace("/auth/login");
+        });
+      }
+
+      if (!roles.includes(decoded.role!)) {
+        toast.error("unauthorised access: access denied");
+        Cookies.remove("accessToken");
+        setTimeout(() => {
+          router.replace("/auth/login");
+        });
+      }
+    }, []);
+
+    return <Component {...props} />;
+  };
+}
+
+export default WithAuthorization;
